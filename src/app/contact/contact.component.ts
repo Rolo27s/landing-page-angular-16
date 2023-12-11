bc8cfa4573f608449056069b691051de82fc9a97
Rolo27s <franrodriguez2727@gmail.com>
2023-12-11 13:56:18 +0100

    Parte basica de formularios lista

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+
+  // Parte para el form tipo plantilla
+  public usuario: any = {
+    nombre: '',
+    email: ''
+  }
+
+  enviarP() {
+    console.log(this.usuario);
+  }
+
+  // Parte para el form tipo reactivo
+  formularioContacto: FormGroup
+
+  constructor( private form: FormBuilder) {
+    this.formularioContacto = this.form.group ({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  hasErrors(controlName: string, errorType: string) {
+    return this.formularioContacto.get(controlName)?.hasError(errorType) && this.formularioContacto.get(controlName)?.touched
+  }
+
+  enviar() {
+    console.log(this.formularioContacto)
+  }
 
 }
